41a201d9e4f27633b46bf6fa9a11795012b50627
Matheus Oliveira Silva <matheussilva@tecgraf.puc-rio.br>
2025-06-08 10:50:05 -0300

    feat: changed llm config type name + built llm config class

--- backend/agent/states.py
+++ backend/agent/states.py
@@ -1,8 +1,8 @@
 from typing import Annotated, TypedDict, List
 from langgraph.graph.message import add_messages
 from langchain_core.documents import Document
-from common.types import LLMConfig
+from common.types import LLMConfigType
 
 class AgentState(TypedDict):
     messages: Annotated[List, add_messages]
-    llm_config: LLMConfig
+    llm_config: LLMConfigType
