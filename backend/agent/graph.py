e4bf070cec63f90ca23e648c1607dc571a174a37
Matheus Oliveira Silva <matheussilva@tecgraf.puc-rio.br>
2025-06-08 17:45:23 -0300

    feat: graph typing and streaming function feat

--- backend/agent/graph.py
+++ backend/agent/graph.py
@@ -1,15 +1,16 @@
 from agent.nodes import AgentNodes
 from agent.states import AgentState
 from langgraph.graph import StateGraph, START, END
+from langgraph.graph.state import CompiledStateGraph
 from langgraph.prebuilt import ToolNode, tools_condition
 from agent.agent_toolbox.toolbox import TOOLS
 
 class Agent:
     def __init__(self):
         self.nodes = AgentNodes()
-        self.graph = self.build_graph(self.nodes)
+        self.graph: CompiledStateGraph = self.build_graph(self.nodes)
     
-    def build_graph(self, nodes: AgentNodes):
+    def build_graph(self, nodes: AgentNodes) -> CompiledStateGraph:
         graph = StateGraph(AgentState)
 
         graph.add_node("agent", nodes.agent_node)
@@ -19,10 +20,16 @@ class Agent:
         graph.add_conditional_edges("agent", tools_condition)  # 2. Usa uma ferramenta ou responde o usuário
         graph.add_edge("tools", "agent")  # 3. Volta para o agente (passo 2)
         
-        return graph
+        return graph.compile()
     
-    def run_agent(self, state: AgentState) -> AgentState:
-        return self.graph.invoke(state)
+    def run_agent(self, agent_input: AgentState) -> AgentState:
+        return self.graph.invoke(agent_input)
     
-    def run_agent_streaming(self, state: AgentState) -> AgentState:
-        pass
+    def run_agent_streaming(self, agent_input: AgentState):
+        for data in self.graph.stream(
+            agent_input,
+            stream_mode="messages"
+        ):
+    
+            yield f"data: {data}\n\n"
+
